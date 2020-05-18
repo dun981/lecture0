b5392509f2bf85db0b8ab73b27804b1ff833ed70
dun981 <63806650+dun981@users.noreply.github.com>
2020-05-18 17:38:41 +0200

    edited

---
+++ For loop.js
@@ -0,0 +1,8 @@
+var myArray = [];
+
+for (var i = 0;i <10; i++) {
+  myArray.push(i);
+}
+
+
+console.log(myArray);
---
+++ record collection.mjs
@@ -0,0 +1,51 @@
+var collection = {
+
+"2548":{
+ "album": "Slippery when wet",
+ "artist": "Bon Jovi",
+ "tracks": [
+    "Let it Rock",
+    "You Give love a Bad Name"
+  ]
+
+},
+
+"2468":{
+  "album": "1999",
+ "artist": "Prince",
+ "tracks": [
+    "1999",
+    "Little Red Corvette"
+  ]
+},
+
+  "1245":{
+  "album": "Robert Palmer",
+  "tracks": [ ]
+
+},
+
+"5439":{
+"album":"ABBA Gold",
+}
+
+};
+
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+
+function  updateRecords (id,prop,value) {
+if (value === ""){
+delete collection [id],[prop];
+
+} else if (prop === "tracks") {
+collection [id][prop] = collection [id][prop] || [];
+collection [id][prop].push(value);
+} else {
+  collection[id][prop] = value;
+}
+
+  return collection;
+}
+updateRecords(2468,"tracks","kim i love you ")
+console.log(updateRecords(5439,"artist","ABBA"));
